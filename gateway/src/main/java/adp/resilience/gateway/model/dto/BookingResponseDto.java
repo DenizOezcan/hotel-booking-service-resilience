219d9647a0c2e7764b6bbc089dd8b3b4acafcbc2
Viany Manuel <jeangmanuel@gmail.com>
2024-03-14 18:15:23 +0800

    Added fixmes for missing parsing implementation

--- gateway/src/main/java/adp/resilience/gateway/model/dto/BookingResponseDto.java
+++ gateway/src/main/java/adp/resilience/gateway/model/dto/BookingResponseDto.java
@@ -3,9 +3,9 @@ package adp.resilience.gateway.model.dto;
 import java.time.LocalDateTime;
 
 public record BookingResponseDto(
-        Long bookingId,
-        Long roomId,
-        String startTime,
-        String endTime,
-        String guestName) {
+                Long bookingId,
+                Long roomId,
+                String startTime, // FIXME
+                String endTime, // FIXME
+                String guestName) {
 }
